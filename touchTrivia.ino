7d0bc0f9d5c0346dc7b2502073fa87f8899fb94b
akwelker <54192150+akwelker@users.noreply.github.com>
2021-03-30 16:41:22 -0600

    repo started

---
+++ touchTrivia.ino
@@ -0,0 +1,160 @@
+/* Adam Welker   BYU  ECEN 301    03/2021
+ *  
+ *  Lab 6 -- touch-trivia
+ *  
+ *  Purpose: Demonstrate the Capacitive Sensor library by making a game
+ *  of trivia operable via homemade touch screen grid
+ *  
+ *  INPUTS: None
+ *  
+ *  OUTPUT: Serial Terminal will display the questions, answers, and final score
+ *  DEPENDENCIES: the CapacitiveSensor Library and Vector Library, available on the library manager in the arduino IDE
+ *  
+ */
+
+#define X1_CAP 30
+#define X2_CAP 50
+#define Y1_CAP 30
+#define Y2_CAP 30
+
+
+#define MODE  1 // if mode is below 0, program is in debug mode
+
+// import libraries and set up grid vars
+
+#include<CapacitiveSensor.h>
+#include<Vector.h>
+
+const int y1_pins[] = {11,8};
+const int y2_pins[] = {11,13};
+
+const int x1_pins[] = {5,7};
+const int x2_pins[] = {5,2};
+
+
+// make Capacitive Sensors
+
+CapacitiveSensor x1 = CapacitiveSensor(x1_pins[0],x1_pins[1]);
+CapacitiveSensor x2 = CapacitiveSensor(x2_pins[0],x2_pins[1]);
+
+CapacitiveSensor y1 = CapacitiveSensor(y1_pins[0],y1_pins[1]);
+CapacitiveSensor y2 = CapacitiveSensor(y2_pins[0],y2_pins[1]);
+
+
+
+// make trivia variables
+
+Vector<String> questions = Vector<String>();
+Vector<char> answers =  Vector<char>();
+int turn;
+int score;
+char ans = 'x';
+ 
+
+
+void setup()
+{
+  // Open a serial connection
+  Serial.begin(9600);
+
+  // add the questions to the vector
+  questions.push_back("What is the Captial of Brazil?\n a.)Manaus\t b.) Sao Paulo\n c.)Rio de Janeiro\t d.)Brasilia");
+  answers.push_back("d");
+  
+  questions.push_back("What is the Captial of Australia?\n a.)Camberra\t b.) Sidney\n c.)Perth\t d.)Adelaide");
+  answers.push_back("a");
+  
+  questions.push_back("What is the Captial of Slovakia?\n a.)Zilina\t b.)Presov\n c.)Bratislava\t d.)Kosice");
+  answers.push_back("c");
+  
+  questions.push_back("What is the Captial of Belgium?\n a.)Antwerp\t b.)Ghent\n c.)Brussels\t d.)Bruges");
+  answers.push_back("c");
+
+  // set the turn and score to 0
+  turn = 0;
+  score = 0;
+
+  Serial.println("Welcome to World Geography Trivia! Let's Begin!");
+  delay(2000);
+}
+
+void loop()
+{
+  // check for debug mode   
+  if(MODE < 0)
+  {
+    //Read the capacitance at each grid value
+    int x1_read = x1.capacitiveSensor(30);
+    int x2_read = x2.capacitiveSensor(30);
+    
+    int y1_read = y1.capacitiveSensor(30);
+    int y2_read = y2.capacitiveSensor(30);
+
+    //print all the values    
+    Serial.print(x1_read);
+    Serial.print('\t');
+    Serial.print(x2_read);
+    Serial.print('\t');
+
+
+    Serial.print(y1_read);
+    Serial.print('\t');
+    Serial.println(y2_read);
+    return;
+  } 
+
+  // ------------------------------------------------------  The trivia implementation begins here
+
+  // The trivia works in three phases until turns % 3 == questions.size()
+  // Phase 1 -- the arudiuno prints the questions
+  // Phase 2 -- the arduino waits for the user to hold a touch screen node
+  // Phase 3 -- the arduino will evaluate the user's choice and add a point if correct. the process then resets
+
+  // if the game is over
+  if(turn > answers.size())
+  {
+    return;
+  }
+  else if(turn == answers.size()) // or is ending 
+  {
+    Serial.print("Thanks for playing your score was: ");
+    Serial.print(score);
+    Serial.print('/');
+    Serial.println(answers.size());
+    turn++;
+  }
+  else if(turn % 3 == 0) // phase 1
+  {
+    Serial.println(questions.at(turn/3)); // print the question, move on!
+    turn++;
+  }
+  else if(turn % 3 == 1) // phase 2
+  {
+    int answerCount = 0;
+
+    while(ans == 'x' || ans == '')
+    {
+      ans = Serial.read();
+    }
+  }
+  else if(turn % 3 == 2) // phase 3
+  {
+    // compare against the real answer
+    bool correct = (ans == answers.at(turn/3));
+    
+    if(correct)
+    {
+      Serial.println("Correct!");
+    }
+    else
+    {
+      Serial.print("Sorry, the answer was: ");
+      Serial.println(answers.at(turn/3));
+    }
+
+    turn++;
+    ans = 'x';
+    delay(3000);
+  }
+    
+}
